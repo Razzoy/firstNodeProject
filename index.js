c75932943b481d7b9a593d6f97e9dfd3faf6a033
Razzoy <1081892@ucn.dk>
2024-05-14 11:06:16 +0200

    opsæt supabase

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { supabase } from './Config/config.supabase.js';
 
 const app = express()
 dotenv.config()
 
-const port = process.env.PORT
+const port = process.env.PORT;
 const api_key = process.env.APIKEY;
 
 app.get('/', (req, res) => {
@@ -14,6 +15,39 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     res.send('Endpoint til POST')
+})
+
+app.get('/sange', async (req, res) => {
+    const { data, error } = await supabase
+        .from('songs')
+        .select('id, title')
+    if(error) {
+        console.error(error);
+    } else {
+        res.send(data);
+    }
+})
+
+app.get('/artists', async (req, res) => {
+    const { data, error } = await supabase
+        .from('artists')
+        .select('id, name, description, image, created_at, updated_at')
+    if(error) {
+        console.error(error);
+    } else {
+        res.send(data);
+    }
+})
+
+app.get('/albums', async (req, res) => {
+    const { data, error } = await supabase
+        .from('albums')
+        .select('title, image, artists(name))')
+    if(error) {
+        console.error(error);
+    } else {
+        res.send(data);
+    }
 })
 
 app.get('/about', (req, res) => {
