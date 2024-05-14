cace64ad476f7592cf9a8dfdc4c7a9b24fd9ec2a
Razzoy <1081892@ucn.dk>
2024-05-14 08:44:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express()
 const port = 3000;
 
 app.get('/', (req, res) => {
+    console.log(req.query);
     res.send('Forside')
+})
+
+app.post('/', (req, res) => {
+    res.send('Endpoint til POST')
 })
 
 app.get('/about', (req, res) => {
@@ -13,4 +18,8 @@ app.get('/about', (req, res) => {
 
 app.listen(port, () =>{
     console.log(`Webserver is running now on http://localhost:${port}`);
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("Siden blev ikke fundet")
 })
