b03c2324b0fad28fe2823f02ecfc569137fa399e
Razzoy <1081892@ucn.dk>
2024-08-05 10:04:09 +0200

    update songcontroller

--- Controllers/song.controller.js
+++ Controllers/song.controller.js
@@ -2,7 +2,9 @@ import express from 'express';
 import SongModel from '../Models/song.model.js'
 export const SongController = express.Router();
 
-SongController.get('/songs', async (req, res) =>{
-    const data = await SongModel.getAllRecords()
-    res.send(data)
+SongController.get('/songs/:id', async (req, res) =>{
+    console.log(req.params);
+    
+    // const data = await SongModel.getAllRecords()
+    // res.send(data)
 })
